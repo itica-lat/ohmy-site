f69c8a6125561996b6452f2093c5985e5ee43bd5
Faku <hola@faku.pro>
2026-04-30 18:31:09 -0300

    fix:

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export PATH="$HOME/.bun/bin:$PATH"
 LIVE=/var/www/ohmy-site
 FLAG=/run/nginx-maintenance/ohmy-site
 
-maintenance_on()  { mkdir -p /run/nginx-maintenance; touch "$FLAG"; sudo nginx -s reload; }
+maintenance_on()  { touch "$FLAG"; sudo nginx -s reload; }
 maintenance_off() { rm -f "$FLAG"; sudo nginx -s reload; }
 trap maintenance_off EXIT
 
